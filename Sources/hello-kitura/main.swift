da433247e22cf340b6444f7c50cd42a2cbbfd48c
Adrian Bolinger <adrian.bolinger@me.com>
2018-09-16 09:56:58 -0400

    Instantiate HeliumLogger and updated port.
    - Instantiate HeliumLogger in main.swift
    - Updated port to 8090

--- Sources/hello-kitura/main.swift
+++ Sources/hello-kitura/main.swift
@@ -2,6 +2,8 @@ import Kitura
 import HeliumLogger
 import LoggerAPI
 
+HeliumLogger.use()
+
 // Create a new router
 let router = Router()
 
@@ -13,7 +15,7 @@ router.get("/") {
 }
 
 // Add an HTTP server and connect it to the router
-Kitura.addHTTPServer(onPort: 8080, with: router)
+Kitura.addHTTPServer(onPort: 8090, with: router)
 
 // Start the Kitura runloop (this call never returns)
 Kitura.run()
