e4c6f14d3dffda3c41324f486cc8be4c8dedd5b7
Adrian Bolinger <adrian.bolinger@me.com>
2018-09-16 11:42:40 -0400

    Returning JSON data lecture

--- Sources/hello-kitura/main.swift
+++ Sources/hello-kitura/main.swift
@@ -7,12 +7,28 @@ HeliumLogger.use()
 // Create a new router
 let router = Router()
 
-// Handle HTTP GET requests to /
-router.get("/") {
-    request, response, next in
-    response.send("Hello, World!")
-    next()
+router.post("hello") { request, response, _ in
+  
 }
+
+router.get("hello") { request, response, next in
+  let pokemons = [["name":"Pikachu"],["name":"Caterpie"]]
+  response.send(json: pokemons)
+
+//  let result = ["status":"ok","message":"Hello World"]
+//  let result = ["message":"Hello World"]
+//  response.send(json: result)
+  next()
+}
+
+//router.get("hello") { request, response, next in
+//  response.send("HELLO WORLD")
+//}
+
+//router.all() { request, response, next in
+//  response.send("Hello World")
+//  try response.end()
+//}
 
 // Add an HTTP server and connect it to the router
 Kitura.addHTTPServer(onPort: 8090, with: router)
